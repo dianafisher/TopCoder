b9610b7f76c607973dd1a4f4f8f3486886790cf6
dianafisher <diana.fisher@gmail.com>
2012-10-24 17:30:25 -0700

    * fix bug where resting for one minute was counted as zero

--- src/com/dianafisher/srm421/div2/GymTraining.java
+++ src/com/dianafisher/srm421/div2/GymTraining.java
@@ -40,35 +40,38 @@ public class GymTraining extends TestCase
         if( !canTrain( minPulse, trainChange, maxPulse) ) return -1;
 
         // how long can we train before maxing out our pulse?
+//        System.out.println("maxPulse = " + maxPulse);
         int pulse = minPulse;
         int totalMinutes = 0;
         int trainingMinutes = 0;
         while( trainingMinutes < needToTrain )
         {
-//            int count = 0;
+            int count = 0;
             while( (pulse + trainChange) <= maxPulse )
             {
                 pulse += trainChange;
+//                System.out.println("pulse = " + pulse);
                 trainingMinutes++;
                 totalMinutes++;
-//                count++;
+                count++;
             }
 //            System.out.println(String.format("trained %d minutes to reach a pulse of %d", count, pulse));
             // how long do we need to rest before we can start training again?
 //            System.out.println("trainingMinutes = " + trainingMinutes);
             if( trainingMinutes == needToTrain ) return totalMinutes;
-//            count = 0;
+            count = 0;
+//            System.out.println("restChange = " + restChange);
             while( (pulse + trainChange) > maxPulse )
             {
                 pulse -= restChange;
+                totalMinutes++;
+                count++;
                 if( pulse < minPulse )
                 {
                     pulse = minPulse;
                     break;
                 }
 //                System.out.println("pulse = " + pulse);
-                totalMinutes++;
-//                count++;
             }
 //            System.out.println(String.format("rested %d minutes to reach a pulse of %d", count, pulse));
         }
@@ -78,9 +81,6 @@ public class GymTraining extends TestCase
 
     private boolean canTrain( int pulse, int delta, int max )
     {
-//        System.out.println("max pulse = " + max);
-        int total = pulse + delta;
-//        System.out.println("pulse after training = " + total);
         return( pulse + delta <= max );
     }
 
@@ -88,7 +88,7 @@ public class GymTraining extends TestCase
     public void test()
     {
         assertEquals(10, trainingTime(5, 70, 120, 25, 15));
-        assertEquals(100, trainingTime(100, 50, 100, 5, 200));
+        assertEquals(109, trainingTime(100, 50, 100, 5, 200));
         assertEquals(-1, trainingTime(1, 60, 70, 11, 11));
         assertEquals(30050, trainingTime(200, 50, 200, 150, 1));
         assertEquals(40, trainingTime(19, 89, 143, 17, 13));
